af3ebfc6ec83557398b4d1ce7d33001aa4ce4c73
Bryan <Bryan.wu.121@gmail.com>
2022-07-23 23:15:48 -0500

    updated methods

--- SinglyLinkedLists.js
+++ SinglyLinkedLists.js
@@ -1,6 +1,6 @@
 // Singly linked lists
 
-// push function
+// push, pop, shift, unshift functions
 
 class Node {
 	constructor (val) {
@@ -27,10 +27,80 @@ class SinglyLinkedList {
         return this
         
     }
+    pop() {
+        if (this.length === 0) return undefined;
+        if (!this.head.next) {
+            this.head = null;
+            this.tail = null;
+            return this
+        let prevNode = this.head;
+        let currentNode = prevNode.next;
+
+        } else {
+            while (currentNode) {
+                if (this.tail == currentNode){
+                    this.tail = prevNode;
+                    prevNode.next = null;
+                    break
+                }
+                prevNode = prevNode.next;
+                currentNode = prevNode.next;
+            }
+        }
+        this.length --;
+        return this
+    }
+    shift() {
+        if (this.length === 0) return undefined;
+        if (!this.head.next) {
+            this.head = null;
+            this.tail = null;
+            return this
+        }
+        this.head = this.head.next;
+
+        this.length --;
+        return this;
+
+    }
+    unshift(val) {
+
+        let newNode = new Node(val);
+        if (this.length === 0 ) {
+            this.head = newNode;
+            this.tail = newNode;
+            return this;
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length ++;
+        return this;
+
+    }
+    get(index) {
+        let currentNode = this.head;
+        let i = 0;
+        if (index<0 || index>this.length-1) return undefined;
+        while (i < index) {
+            currentNode = currentNode.next;
+            i++;
+        }
+        return currentNode;
+
+    }
+    set(value, index) {
+        let currentNode = this.get(index)
+        if (currentNode) {
+            currentNode.val = value;
+        } else return undefined
+
+        return this;
+    }
 }
 
 let linkedList = new SinglyLinkedList()
 linkedList.push(2)
 linkedList.push(11)
 linkedList.push(13)
+console.log(linkedList.set(100,3))
 console.log(linkedList)
