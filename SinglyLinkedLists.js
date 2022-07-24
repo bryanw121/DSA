a68d3c416c5bafeae999b3f2f51fc8edeb69fa16
Bryan <Bryan.wu.121@gmail.com>
2022-07-24 01:23:15 -0500

    adding more methods

--- SinglyLinkedLists.js
+++ SinglyLinkedLists.js
@@ -1,6 +1,6 @@
 // Singly linked lists
 
-// push, pop, shift, unshift functions
+// push, pop, shift, unshift, insert, remove, and reverse functions
 
 class Node {
 	constructor (val) {
@@ -33,10 +33,9 @@ class SinglyLinkedList {
             this.head = null;
             this.tail = null;
             return this
-        let prevNode = this.head;
-        let currentNode = prevNode.next;
-
         } else {
+            let prevNode = this.head;
+            let currentNode = prevNode.next;
             while (currentNode) {
                 if (this.tail == currentNode){
                     this.tail = prevNode;
@@ -92,15 +91,72 @@ class SinglyLinkedList {
         let currentNode = this.get(index)
         if (currentNode) {
             currentNode.val = value;
-        } else return undefined
+            return true;
+        } else return false;
+    }
+    insert(value, index) {
+        if (index === 0) return !!this.unshift(value);
+        else if (index === this.length) return !!this.push(value);
+        if (index<0 || index>this.length) return false;
+        else {
+            let newNode = new Node(value)
+            let prevNode = this.get(index-1)
+            let tempNode = prevNode.next
+            prevNode.next = newNode;
+            newNode.next = tempNode;
+            this.length ++;
+            return true;
+        }
+    }
+    remove(index) {
+        if (index === 0) return !!this.shift();
+        else if (index === this.length-1) return !!this.pop();
+        if (index<0 || index>=this.length) return false;
+        else {
+            let prevNode = this.get(index-1);
+            let tempNode = prevNode.next;
+            prevNode.next = tempNode.next;
+        }
+        this.length --;
+        return true;
 
+    }
+    reverse() {
+        [this.head, this.tail] = [this.tail, this.head];
+        let prevNode = this.tail
+        let currentNode = prevNode.next;
+        this.tail.next = null;
+        
+        
+        for (let i=0;i<this.length-1; i++) {
+            
+            let tempNode = currentNode.next;
+            currentNode.next = prevNode;
+            prevNode = currentNode;
+            currentNode = tempNode;
+        }
+        this.print(this)
+        
         return this;
     }
+
+    print() {
+        let array = [];
+        let current = this.head;
+        while (current) {
+            array.push(current.val);
+            current = current.next;
+        }
+        console.log(array)
+    }
+
+
 }
 
 let linkedList = new SinglyLinkedList()
 linkedList.push(2)
 linkedList.push(11)
 linkedList.push(13)
-console.log(linkedList.set(100,3))
-console.log(linkedList)
+linkedList.push(4)
+console.log(linkedList.reverse())
+// console.log(linkedList)
