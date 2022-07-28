2a5105e6230bc77e6eede719a43c7a71b6b2ff46
Bryan <Bryan.wu.121@gmail.com>
2022-07-27 19:17:51 -0500

    binary search trees

--- DoublyLinkedLists.js
+++ DoublyLinkedLists.js
@@ -64,6 +64,91 @@ class DoublyLinkedList {
         return tempNode;
 
     }
+    unshift(val) {
+        newNode = new Node(val);
+        if (this.length===0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length ++;
+        return this;
+    }
+    get(index) {
+        if (index<0 || index>this.length-1) return false;
+        if (index < this.length/2){
+            let count = 0;
+            let current = this.head;
+            while (count != index) {
+                current = current.next;
+                count++;
+            }
+            return current;
+        } else {
+            let count = this.length - 1;
+            let current = this.tail;
+            while (count!= index) {
+                current = current.prev;
+                count--;
+            }
+            return current;
+        }
+    }
+    set(value, index) {
+        let currentNode = this.get(index)
+        currentNode.val = value;
+        return !!this.get(index)
+    }
+    insert(value, index) {
+        if (index<0 || index>this.length) return false;
+        if (index === this.length) return !!this.push(value);
+        if (index === 0) return !!this.unshift(value);
+
+        let newNode = new Node(value);
+        let prevNode = this.get(index-1);
+        let tempNode = prevNode.next;
+
+        prevNode.next = newNode, newNode.prev = prevNode;
+        newNode.next = tempNode, tempNode.prev = newNode;
+        this.length++;
+        return true;
+    }
+    remove(index) {
+        if (index<0 || index >= this.length) return false;
+        if (index === this.length-1) return this.pop();
+        if (index === 0) return this.shift();
+
+        else {
+            let prevNode = this.get(index-1);
+            let currentNode = prevNode.next;
+            let tempNode = currentNode.next;
+
+            prevNode.next = tempNode, tempNode.prev = prevNode;
+            currentNode.prev = null, currentNode.next = null;
+
+            this.length--;
+            return currentNode;
+        }
+    }
+    reverse(){
+        let currentNode = this.head;
+        let prevNode = null;
+        let tempNode;
+        
+        while (currentNode) {
+            tempNode = currentNode.next;
+            if (prevNode) prevNode.prev = currentNode;
+            currentNode.next = prevNode;
+            prevNode = currentNode;
+            currentNode = tempNode;
+        }
+        [this.head, this.tail] = [this.tail, this.head];
+        return this;
+        
+    }
 }
 
 let list = new DoublyLinkedList;
@@ -71,5 +156,6 @@ let list = new DoublyLinkedList;
 list.push(3)
 list.push(5)
 list.push(6)
-list.pop()
+list.insert(10, 1)
+list.remove(1)
 console.log(list)
